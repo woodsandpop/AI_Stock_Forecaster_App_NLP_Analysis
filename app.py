4376bce0631a81e7c02596d835a379116bb4964b
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-05-22 13:31:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
-
 import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from GoogleNews import GoogleNews
 from newspaper import Article
 from newspaper import Config
 from wordcloud import WordCloud, STOPWORDS
 
-nltk.download('vader_lexicon') #required for Sentiment Analysis
 
 now = dt.date.today()
 now = now.strftime('%m-%d-%Y')
@@ -23,16 +21,9 @@ config = Config()
 config.browser_user_agent = user_agent
 config.request_timeout = 10
 
-# Streamlit Dashboard          
-st.set_page_config(page_title ="Woods and Pop Ltd", page_icon =":guardsman:", layout ="centered")
-st.image("logo.jpeg", width = 600)
-st.title('Woods and Pop Ltd')
-st.header('🔎AI Stock Forecaster App')
-st.subheader("NLP Analysis of the selected Stock using Stock articles")
-
 # save the company name in a variable
-company_name = st.text_input("Please provide the name of the Company: ")
-#As long as the company name is valid, not empty...
+company_name = st.text_input("Please provide the name of the Company or a Ticker: ")
+#As long as the company name is valid not empty...
 if company_name != '':
     st.write(f'Searching for and analyzing {company_name}, Please be patient, it might take a while...')
 
@@ -42,7 +33,7 @@ if company_name != '':
     result = googlenews.result()
     #store the results
     df = pd.DataFrame(result)
-    st.write(df)
+    # print(df)
 
     try:
         list =[] #creating an empty list 
@@ -50,11 +41,11 @@ if company_name != '':
             dict = {} #creating an empty dictionary to append an article in every single iteration
             article = Article(df['link'][i],config=config) #providing the link
             try:
-            article.download() #downloading the article 
-            article.parse() #parsing the article
-            article.nlp() #performing natural language processing (nlp)
+              article.download() #downloading the article 
+              article.parse() #parsing the article
+              article.nlp() #performing natural language processing (nlp)
             except:
-                pass 
+               pass 
             #storing results in our empty dictionary
             dict['Date']=df['date'][i] 
             dict['Media']=df['media'][i]
@@ -66,8 +57,8 @@ if company_name != '':
         check_empty = not any(list)
         # print(check_empty)
         if check_empty == False:
-        news_df=pd.DataFrame(list) #creating dataframe
-        st.write(news_df)
+          news_df=pd.DataFrame(list) #creating dataframe
+          st.write(news_df)
 
     except Exception as e:
         #exception handling
@@ -75,21 +66,22 @@ if company_name != '':
         st.write('Looks like, there is some error in retrieving the data, Please try again or try with a different ticker.' )
 
     
-     #Sentiment Analysis
-    def percentage(part,whole):
-        return 100 * float(part)/float(whole)
+    # Sentiment Analysis
+    def percentage(part, whole):
+        return 100 * float(part) / float(whole)
 
-    #Assigning Initial Values
+
+    # Assigning Initial Values
     positive = 0
     negative = 0
     neutral = 0
-    #Creating empty lists
+    # Creating empty lists
     news_list = []
     neutral_list = []
     negative_list = []
     positive_list = []
 
-    #Iterating over the tweets in the dataframe
+    # Iterating over the tweets in the dataframe
     for news in news_df['Summary']:
         news_list.append(news)
         analyzer = SentimentIntensityAnalyzer().polarity_scores(news)
@@ -99,50 +91,55 @@ if company_name != '':
         comp = analyzer['compound']
 
         if neg > pos:
-            negative_list.append(news) #appending the news that satisfies this condition
-            negative += 1 #increasing the count by 1
+            negative_list.append(news)  # appending the news that satisfies this condition
+            negative += 1  # increasing the count by 1
         elif pos > neg:
-            positive_list.append(news) #appending the news that satisfies this condition
-            positive += 1 #increasing the count by 1
+            positive_list.append(news)  # appending the news that satisfies this condition
+            positive += 1  # increasing the count by 1
         elif pos == neg:
-            neutral_list.append(news) #appending the news that satisfies this condition
-            neutral += 1 #increasing the count by 1 
+            neutral_list.append(news)  # appending the news that satisfies this condition
+            neutral += 1  # increasing the count by 1
 
-    positive = percentage(positive, len(news_df)) #percentage is the function defined above
+    positive = percentage(positive, len(news_df))  # percentage is the function defined above
     negative = percentage(negative, len(news_df))
     neutral = percentage(neutral, len(news_df))
 
-    #Converting lists to pandas dataframe
+    # Converting lists to pandas dataframe
     news_list = pd.DataFrame(news_list)
     neutral_list = pd.DataFrame(neutral_list)
     negative_list = pd.DataFrame(negative_list)
     positive_list = pd.DataFrame(positive_list)
-    #using len(length) function for counting
+    # using len(length) function for counting
     st.write("Positive Sentiment:", '%.2f' % len(positive_list), end='\n')
     st.write("Neutral Sentiment:", '%.2f' % len(neutral_list), end='\n')
     st.write("Negative Sentiment:", '%.2f' % len(negative_list), end='\n')
 
-    #Creating PieChart
-    labels = ['Positive ['+str(round(positive))+'%]' , 'Neutral ['+str(round(neutral))+'%]','Negative ['+str(round(negative))+'%]']
+    # Creating PieCart
+
+    labels = ['Positive [' + str(round(positive)) + '%]', 'Neutral [' + str(round(neutral)) + '%]',
+              'Negative [' + str(round(negative)) + '%]']
     sizes = [positive, neutral, negative]
-    colors = ['yellowgreen', 'blue','red']
-    patches, texts = plt.pie(sizes,colors=colors, startangle=90)
+    colors = ['yellowgreen', 'blue', 'red']
+    patches, texts = plt.pie(sizes, colors=colors, startangle=90)
     plt.style.use('default')
     plt.legend(labels)
-    plt.title("Sentiment Analysis Result for selected stock = "+company_name+"" )
+    plt.title("Sentiment Analysis Result for stock= " + company_name + "")
     plt.axis('equal')
-    st.pyplot(plt)
+    st.plt.show()
 
-    # Word cloud visualization
+
+    # word cloud visualization
     def word_cloud(text):
         stopwords = set(STOPWORDS)
         allWords = ' '.join([nws for nws in text])
-        wordCloud = WordCloud(background_color='black',width = 1600, height = 800,stopwords = stopwords,min_font_size = 20,max_font_size=150,colormap='prism').generate(allWords)
-        fig, ax = plt.subplots(figsize=(20,10), facecolor='k')
+        wordCloud = WordCloud(background_color='black', width=1600, height=800, stopwords=stopwords, min_font_size=20,
+                              max_font_size=150, colormap='prism').generate(allWords)
+        fig, ax = plt.subplots(figsize=(20, 10), facecolor='k')
         plt.imshow(wordCloud)
         ax.axis("off")
         fig.tight_layout(pad=0)
-        st.plt(fig)
-    
-    st.write('Wordcloud for ' + company_name)
-    word_cloud(news_df['Summary'].values)   
+        st.plt.show()
+
+
+    print('Wordcloud for ' + company_name)
+    word_cloud(news_df['Summary'].values)
