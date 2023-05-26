3647c753b3caeeaf1d88adb8ac2f89c5225940bc
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-05-26 10:54:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ if company_name != '':
     negative_list = pd.DataFrame(negative_list)
     positive_list = pd.DataFrame(positive_list)
     #using len(length) function for counting
-    st.write("BUY SIDE:",'%*100' % len(positive_list), end='\n')
+    st.write("BUY SIDE:",'%' % len(positive_list*100), end='\n')
     st.write("NEUTRAL SIDE:", '%.2f' % len(neutral_list), end='\n')
     st.write("SELL SIDE:", '%.2f' % len(negative_list), end='\n')
 
